0db83c3e3da8ebebb0580fd83d49a3bfbf965873
Luiz_Buarque_Dev <143226652+LuizBuarqueDev@users.noreply.github.com>
2025-09-28 00:20:31 -0300

    Criação da branch

--- src/main/java/br/edu/ifpe/pontoif/pontoif/controller/FrequencyController.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/controller/FrequencyController.java
@@ -1,4 +1,17 @@
 package br.edu.ifpe.pontoif.pontoif.controller;
 
+import br.edu.ifpe.pontoif.pontoif.service.FrequencyService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/frequency")
+@Tag(name="Frequency management Endpoints",description = "API Endpoints to manage course")
 public class FrequencyController {
+
+    private final FrequencyService frequencyService;
+
 }
--- src/main/java/br/edu/ifpe/pontoif/pontoif/service/FrequencyService.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/service/FrequencyService.java
@@ -1,4 +1,10 @@
 package br.edu.ifpe.pontoif.pontoif.service;
 
+import br.edu.ifpe.pontoif.pontoif.repository.FrequencyRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class FrequencyService {
+    private FrequencyRepository frequencyRepository;
 }
