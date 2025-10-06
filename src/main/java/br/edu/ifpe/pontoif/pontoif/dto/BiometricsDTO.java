1b483630eacbf40c352104856c78a55118c63ac9
Luiz_Buarque_Dev <143226652+LuizBuarqueDev@users.noreply.github.com>
2025-10-06 00:13:00 -0300

    Add `BiometricsDTO` class with `id`, `user`, and `template` properties

--- src/main/java/br/edu/ifpe/pontoif/pontoif/dto/BiometricsDTO.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/dto/BiometricsDTO.java
@@ -1,4 +1,14 @@
 package br.edu.ifpe.pontoif.pontoif.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class BiometricsDTO {
+    private Long id;
+    private UUID user;
+    private String template;
 }
