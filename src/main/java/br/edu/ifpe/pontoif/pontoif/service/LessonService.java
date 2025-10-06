6c147906a5acd8408120a838909a75a14d772400
Luiz_Buarque_Dev <143226652+LuizBuarqueDev@users.noreply.github.com>
2025-10-05 22:05:17 -0300

    Add `LessonService` and `LessonMapper` for lesson management functionality

---
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/service/LessonService.java
@@ -0,0 +1,45 @@
+package br.edu.ifpe.pontoif.pontoif.service;
+
+import br.edu.ifpe.pontoif.pontoif.dto.LessonDTO;
+import br.edu.ifpe.pontoif.pontoif.mapper.LessonMapper;
+import br.edu.ifpe.pontoif.pontoif.repository.LessonRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class LessonService {
+    //TODO: Implementar update
+
+    private final LessonRepository lessonRepository;
+    private final LessonMapper lessonMapper;
+
+    @Transactional
+    public void insertLesson(final LessonDTO lessonDTO) {
+        lessonRepository.save(lessonMapper.toEntity(lessonDTO));
+    }
+
+    public Optional<LessonDTO> getLessonById(final UUID uuid) {
+        return lessonRepository.findById(uuid).map(lessonMapper::toDTO);
+    }
+
+    public List<LessonDTO> getAllLessons() {
+        return lessonRepository.findAll()
+                .stream()
+                .map(lessonMapper::toDTO)
+                .toList();
+    }
+
+    @Transactional
+    public boolean deleteLesson(final UUID uuid) {
+        return lessonRepository.findById(uuid).map(lesson -> {
+            lessonRepository.delete(lesson);
+            return true;
+        }).orElse(false);
+    }
+}
