41ba3e04e758a98b34ec42227ca6a8c718102c21
Luiz_Buarque_Dev <143226652+LuizBuarqueDev@users.noreply.github.com>
2025-10-12 11:09:15 -0300

    Add `fromId` and `toId` methods to `LessonMapper` for UUID conversion

--- src/main/java/br/edu/ifpe/pontoif/pontoif/mapper/LessonMapper.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/mapper/LessonMapper.java
@@ -5,14 +5,19 @@ import br.edu.ifpe.pontoif.pontoif.entity.Lesson;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.UUID;
+
 @Mapper(componentModel = "spring", uses = { RecordMapper.class })
 public interface LessonMapper {
 
     @Mapping(target = "discipline.id", source = "discipline")
     @Mapping(target = "records", source = "records")
-    Lesson toEntity(LessonDTO lessonDTO);
+    Lesson toEntity(LessonDTO dto);
 
     @Mapping(target = "discipline", source = "discipline.id")
     @Mapping(target = "records", source = "records")
-    LessonDTO toDTO(Lesson lesson);
+    LessonDTO toDTO(Lesson entity);
+
+    Lesson fromId(UUID id);
+    UUID toId(Lesson lesson);
 }
