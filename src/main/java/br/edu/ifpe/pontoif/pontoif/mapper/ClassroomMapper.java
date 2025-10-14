f5e58be63deb351ca9e6e4f072a1dac049f6d25a
Marcos Suel <marcosuelvs@hotmail.com>
2025-10-14 12:20:34 -0300

    add crud classroom

--- src/main/java/br/edu/ifpe/pontoif/pontoif/mapper/ClassroomMapper.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/mapper/ClassroomMapper.java
@@ -2,17 +2,64 @@ package br.edu.ifpe.pontoif.pontoif.mapper;
 
 import br.edu.ifpe.pontoif.pontoif.dto.ClassroomDTO;
 import br.edu.ifpe.pontoif.pontoif.entity.Classroom;
-import org.mapstruct.Mapper;
+import br.edu.ifpe.pontoif.pontoif.entity.Course;
+import br.edu.ifpe.pontoif.pontoif.entity.Discipline;
+import org.mapstruct.*;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ClassroomMapper {
 
+
+    @Mapping(target = "course", source = "courseId", qualifiedByName = "mapIdToCourse")
     Classroom toEntity(ClassroomDTO classroomDTO);
 
+    @Mapping(target = "courseId", source = "course.id")
     ClassroomDTO toDTO(Classroom classroom);
 
-    Classroom fromId(UUID id);
-    UUID toId(Classroom classroom);
+
+    @Named("mapIdToCourse")
+    default Course mapIdToCourse(UUID id) {
+        if (id == null) return null;
+        Course course = new Course();
+        course.setId(id);
+        return course;
+    }
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(ClassroomDTO classroomDTO, @MappingTarget Classroom classroom);
+
+    default Classroom fromId(UUID id) {
+        if (id == null) return null;
+        Classroom classroom = new Classroom();
+        classroom.setId(id);
+        return classroom;
+    }
+
+    default UUID toId(Classroom classroom) {
+        return classroom != null ? classroom.getId() : null;
+    }
+
+
+    @Named("mapIdsToDisciplines")
+    default List<Discipline> mapIdsToDisciplines(List<UUID> ids) {
+        if (ids == null) return null;
+        return ids.stream().map(id -> {
+            Discipline discipline = new Discipline();
+            discipline.setId(id);
+            return discipline;
+        }).collect(Collectors.toList());
+    }
+
+
+    @Named("mapDisciplinesToIds")
+    default List<UUID> mapDisciplinesToIds(List<Discipline> disciplines) {
+        if (disciplines == null) return null;
+        return disciplines.stream()
+                .map(Discipline::getId)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/br/edu/ifpe/pontoif/pontoif/service/ClassroomService.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/service/ClassroomService.java
@@ -1,8 +1,12 @@
 package br.edu.ifpe.pontoif.pontoif.service;
 
 import br.edu.ifpe.pontoif.pontoif.dto.ClassroomDTO;
+import br.edu.ifpe.pontoif.pontoif.dto.LessonDTO;
+import br.edu.ifpe.pontoif.pontoif.entity.Classroom;
+import br.edu.ifpe.pontoif.pontoif.entity.Discipline;
 import br.edu.ifpe.pontoif.pontoif.mapper.ClassroomMapper;
 import br.edu.ifpe.pontoif.pontoif.repository.ClassroomRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -36,10 +41,32 @@ public class ClassroomService {
     }
 
     @Transactional
+    public List<UUID> getDisciplineIdsByClassroomId(UUID classroomId) {
+        Classroom classroom = classroomRepository.findById(classroomId)
+                .orElseThrow(() -> new EntityNotFoundException("Classroom not found: " + classroomId));
+
+        return classroom.getDisciplines()
+                .stream()
+                .map(Discipline::getId)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
     public boolean deleteClassroom(final UUID uuid) {
         return classroomRepository.findById(uuid).map(classroom -> {
             classroomRepository.delete(classroom);
             return true;
         }).orElse(false);
+    }
+
+    @Transactional
+    public Optional<ClassroomDTO> updateClassroom(final UUID uuid, final ClassroomDTO classroomDTO) {
+        return classroomRepository.findById(uuid)
+            .map(existingClassroom -> {
+                classroomMapper.updateEntityFromDto(classroomDTO, existingClassroom);
+                Classroom updatedClassroom = classroomRepository.save(existingClassroom);
+                return classroomMapper.toDTO(updatedClassroom);
+            }
+        );
     }
 }
