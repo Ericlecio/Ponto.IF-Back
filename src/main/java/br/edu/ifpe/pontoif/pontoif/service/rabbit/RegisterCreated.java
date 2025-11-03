2a1719152a063971bd7da52c27c948d835bf34db
jadeilson <jadeilsonm17@gmail.com>
2025-11-03 15:55:55 -0300

    feat: update user registration process with logging and error handling

--- src/main/java/br/edu/ifpe/pontoif/pontoif/service/rabbit/RegisterCreated.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/service/rabbit/RegisterCreated.java
@@ -8,28 +8,40 @@ import br.edu.ifpe.pontoif.pontoif.repository.UserRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 @Component
 public class RegisterCreated {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final Logger logger;
 
     public RegisterCreated(final UserRepository userRepository,
                            final UserMapper userMapper) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        logger = Logger.getLogger(RegisterCreated.class.getName());
     }
 
     @RabbitListener(queues = RabbitConfig.QUEUE_REGISTER_CREATED)
     public void receiveMessage(UserDTO message) {
-        if (userRepository.existsByCorrelationId(message.getId()))
-            return;
-        var user = userMapper.toEntity(message);
-        user.setIsActive(true);
-        if (message.getRole() == null) {
-            user.setRole(Role.STUDENT);
+        logger.log(Level.INFO, "Received Register Created message for user ID: {}" + message);
+        try {
+            if (userRepository.existsByCorrelationId(message.getId()))
+                return;
+            var user = userMapper.toEntity(message);
+            user.setIsActive(true);
+            user.setType(Role.STUDENT.name());
+            if (message.getRole() == null) {
+                user.setRole(Role.STUDENT);
+                user.setType(message.getRole().name());
+            }
+            userRepository.save(user);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error: {}" + e.getMessage());
         }
-        userRepository.save(user);
     }
 }
