570b6471b7bc343471ac45fd03c5d16f0b9487b0
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2025-11-23 14:16:54 -0300

    Annotate `@PathVariable` for `id` in `getTeacherById` endpoint in UserController.

--- src/main/java/br/edu/ifpe/pontoif/pontoif/controller/UserController.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/controller/UserController.java
@@ -1,14 +1,13 @@
 package br.edu.ifpe.pontoif.pontoif.controller;
 
-import br.edu.ifpe.pontoif.pontoif.dto.SubjectDTO;
 import br.edu.ifpe.pontoif.pontoif.dto.SubjectOfferingDTO;
 import br.edu.ifpe.pontoif.pontoif.dto.UserDTO;
-import br.edu.ifpe.pontoif.pontoif.entity.User;
 import br.edu.ifpe.pontoif.pontoif.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +28,7 @@ public class UserController {
     }
 
     @GetMapping("/teacher/{id}")
-    public ResponseEntity<UserDTO> getTeacherById(UUID id) {
+    public ResponseEntity<UserDTO> getTeacherById(@PathVariable UUID id) {
         return userService.getTeacherById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
