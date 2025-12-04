c0202f9e9c1e9966a52cc14409f862e33cf24bd9
Jadeilson Silva <jadeilsonm17@gmail.com>
2025-12-04 09:08:18 -0300

    fix: remove development profile annotation from DevSecurityConfig

--- src/main/java/br/edu/ifpe/pontoif/pontoif/configurations/DevSecurityConfig.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/configurations/DevSecurityConfig.java
@@ -3,7 +3,6 @@ package br.edu.ifpe.pontoif.pontoif.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,7 +15,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@Profile("dev")
 public class DevSecurityConfig {
 
     @Autowired
