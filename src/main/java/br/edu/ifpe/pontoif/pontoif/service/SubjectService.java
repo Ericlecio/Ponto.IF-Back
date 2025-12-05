3ad2048719679f7b3d4761a984a5e1244e3f5dff
Jadeilson Silva <jadeilsonm17@gmail.com>
2025-12-05 14:45:43 -0300

    feat: update SubjectService to optimize course association handling in update method

--- src/main/java/br/edu/ifpe/pontoif/pontoif/service/SubjectService.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/service/SubjectService.java
@@ -12,10 +12,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -63,7 +60,8 @@ public class SubjectService {
             List<Course> courses = getCourses(dto);
             var existingCourses = existing.getCourses();
             existingCourses.addAll(courses);
-            existing.setCourses(existingCourses);
+            var toRemove = new HashSet<>(existingCourses);
+            existing.setCourses(toRemove.stream().toList());
             repository.save(existing);
             return mapper.toDTO(existing);
         });
