bb6a3e0eaf6b0c768c6b3eb4cd5e6b45a86a52bc
Jadeilson Silva <jadeilsonm17@gmail.com>
2025-12-05 14:30:12 -0300

    feat: introduce SubjectResponseDTO and update SubjectController, SubjectService, and SubjectMapper to use it

--- src/main/java/br/edu/ifpe/pontoif/pontoif/dto/SubjectResponseDTO.java
+++ src/main/java/br/edu/ifpe/pontoif/pontoif/dto/SubjectResponseDTO.java
@@ -3,10 +3,18 @@ package br.edu.ifpe.pontoif.pontoif.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubjectResponseDTO {
     private UUID id;
     @NotNull
